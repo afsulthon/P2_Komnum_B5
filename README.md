01dcfb09bf364948fb2c8a6c765b676f312b7041
Thariq Agfi Hermawan <thoriq.aghfi60@gmail.com>
2022-12-20 21:09:03 +0700

    feat: add function f(x)

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # P2_Komnum_B5
 Praktikum 2 Komputasi Numerik
+
+- Thariq Agfi Hermawan
+- Akmal Sulthon F
+- Tigo S Yoga
---
+++ Romberg.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+def f(x):
+  return np.exp(x**2) + 2*x
+
+x = input()
+y = input()
+
+print(x, y)
